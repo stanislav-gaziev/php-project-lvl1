052fc63c91fde669f501fdbfe0033b4d0b453306
Stas <stasstas94@mail.ru>
2021-01-30 19:15:32 +0300

    fix brain-games Cli.php for automatic check

--- src/Cli.php
+++ src/Cli.php
@@ -14,34 +14,9 @@ function getPlayerName(): string
     return $name;
 }
 
-function chooseGame(string $gameName = ''): void
+function chooseGame(string $gameName): void
 {
     $playerName = getPlayerName();
 
-    if ($gameName === '') {
-        line('Games:');
-
-        $dir = __DIR__ . '/Games';
-        $files = array_diff(scandir($dir), array('..', '.'));
-        $games = [];
-        $num = 1;
-
-        foreach ($files as $file) {
-            [$fileName] = explode('.', $file);
-            $games[$num] = $fileName;
-            line('%s. %s', $num, $fileName);
-            $num += 1;
-        }
-
-        $gameNumber = prompt('Write number a game (example: 1)');
-        $gameName = ($games[$gameNumber] ?? '');
-    } else {
-        $games = [$gameName];
-    }
-
-    if (in_array($gameName, $games, true)) {
-        toPlayGame($playerName, $gameName);
-    } else {
-        line('Incorrect number a game!');
-    }
+    toPlayGame($playerName, $gameName);
 }
